72f5b9389e9493ec08bd5d9c4ccf9232ae575d5b
jinmo2482 <116600690+jinmo2482@users.noreply.github.com>
2026-01-22 18:24:48 +0800

    Fix bottom navigation to stay anchored

---
+++ app/src/main/java/com/example/groundcontrolapp/ExploreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.groundcontrolapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExploreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explore)
+    }
+}
